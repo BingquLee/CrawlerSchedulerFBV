58be1872416ccf163a5724a05354e8e01a8903c3
BingquLee <bingqulee@gmail.com>
2019-11-20 15:53:37 +0800

    BingquLee

--- Crons/GetJobs.py
+++ Crons/GetJobs.py
@@ -28,34 +28,30 @@ def get_jobs():
     conn.commit()
     for job in job_list:
         print(job)
-        if job["channel"] == 'Tiktok':
-            #sql_update_job = "UPDATE jobs SET status=1 WHERE id='{}'".format(job[0])
-            #cursor.execute(sql_update_job)
-            #conn.commit()
-            file_error_count = 0
 
-            file_sql = "SELECT * FROM files WHERE status=0 AND account='{}' LIMIT {}".format(job["account"], job["file_amount"])
-            cursor.execute(file_sql)
-            file_list = cursor.fetchall()
-            for file in file_list:
-                # print('file_name', r'Videos/{}/To_{}/{}.mp4'.format(file[1], file[2], file[0]))
-                try:
-                    video_uploader(job["account"], r'Videos/{}/To_{}/{}.mp4'.format(file["channel"], file["account"], file["id"]), job["text"])
-                    file_update_sql = "UPDATE files SET status=1 WHERE id='{}';".format(file["id"])
-                    cursor.execute(file_update_sql)
-                    conn.commit()
-                except Exception as e:
-                    file_update_sql = "UPDATE files SET status=-1 WHERE id='{}';".format(file["id"])
-                    cursor.execute(file_update_sql)
-                    conn.commit()
-                    # raise e
-                    file_error_count -= 1
-                    print('file_name', r'Videos/{}/To_{}/{}.mp4'.format(file["channel"], file["account"], file["id"]))
-                    continue
-            if file_error_count != 0:
-                sql_update_job = "UPDATE jobs SET status={} WHERE id='{}'".format(file_error_count, job["id"])
-                cursor.execute(sql_update_job)
+        file_error_count = 0
+        file_sql = "SELECT * FROM files WHERE status=0 AND channel='{}' AND account='{}' LIMIT {}".format(job["channel"], job["account"], job["file_amount"])
+        cursor.execute(file_sql)
+        file_list = cursor.fetchall()
+        for file in file_list:
+            # print('file_name', r'Videos/{}/To_{}/{}.mp4'.format(file[1], file[2], file[0]))
+            try:
+                video_uploader(job["account"], r'Videos/{}/To_{}/{}.mp4'.format(file["channel"], file["account"], file["id"]), job["text"])
+                file_update_sql = "UPDATE files SET status=1 WHERE id='{}';".format(file["id"])
+                cursor.execute(file_update_sql)
                 conn.commit()
+            except Exception as e:
+                file_update_sql = "UPDATE files SET status=-1 WHERE id='{}';".format(file["id"])
+                cursor.execute(file_update_sql)
+                conn.commit()
+                # raise e
+                file_error_count -= 1
+                print('file_name', r'Videos/{}/To_{}/{}.mp4'.format(file["channel"], file["account"], file["id"]))
+                continue
+        if file_error_count != 0:
+            sql_update_job = "UPDATE jobs SET status={} WHERE id='{}'".format(file_error_count, job["id"])
+            cursor.execute(sql_update_job)
+            conn.commit()
 
     cursor.close()
 
